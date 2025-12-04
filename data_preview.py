5eb37c42dd7e32f139d71c0b3ff5463c32c11638
marcus trinder <m.trinder@ortus.co.uk>
2025-12-04 11:20:26 +0000

    initial commit

--- data_preview.py
+++ data_preview.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-def preview_tf_idf(path):
+def preview_tf_idf_top(path):
+    # Function to display top scores for each document in dataframe format.
 
     tf_idf = pd.read_csv(path)
 
@@ -9,5 +10,19 @@ def preview_tf_idf(path):
         .groupby("document")
         .head(5)
     )
-
+    print("Top 5 scores per document.")
     print(top5)
+
+def preview_tf_idf_bottom(path):
+    # Function to display top scores for each document in dataframe format.
+
+    tf_idf = pd.read_csv(path)
+
+    bottom5 = (
+        tf_idf.sort_values(["document", "tfidf"], ascending=[True, False])
+        .groupby("document")
+        .tail(5)
+    )
+    print("Bottom 5 scores per document.")
+
+    print(bottom5)
